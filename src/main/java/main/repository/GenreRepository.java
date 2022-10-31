a1a55468d053bd921cd555b58d62d28b216293d1
Yuliia <yulia.karnoza@gmail.com>
2022-10-31 13:08:38 -0400

    created CinemaRepository

---
+++ src/main/java/main/repository/GenreRepository.java
@@ -0,0 +1,16 @@
+package main.repository;
+
+import com.cydeo.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository <Genre, Long> {
+
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that return all genres
+
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query that returns genres by containing name
+}
